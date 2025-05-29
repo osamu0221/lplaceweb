8bfcfaf5dd3eb5a8595f46126fa424b8e0ce3127
おさむ <zzzstanaka@gmail.com>
2025-05-30 01:50:56 +0900

    🔄 Webアプリ更新

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4127241687"
+    serviceWorkerVersion: "2420126599"
   }
 });
