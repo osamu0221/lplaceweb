78a9a115e8e279756884d7180ff8943f8bef69eb
おさむ <zzzstanaka@gmail.com>
2025-05-30 13:45:54 +0900

    📦 main/docs にアップロード

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "485138090"
+    serviceWorkerVersion: "2404368479"
   }
 });
