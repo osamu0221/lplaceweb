e58ebec867f5c259c6ab040583d0f8b07ff245f1
おさむ <zzzstanaka@gmail.com>
2025-05-30 17:10:32 +0900

    🔧 デバッグ用更新

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2459799648"
+    serviceWorkerVersion: "3618020924"
   }
 });
