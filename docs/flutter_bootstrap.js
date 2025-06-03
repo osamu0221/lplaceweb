74543bced87811cbf0ae3c1783b955f387a09b14
おさむ <zzzstanaka@gmail.com>
2025-06-03 12:47:55 +0900

    🔁 lplace_debug に再アップ

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4019081940"
+    serviceWorkerVersion: "1404673724"
   }
 });
