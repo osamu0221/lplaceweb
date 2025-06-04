02c6a112fefba53fb321280317219e9804dcdef2
おさむ <zzzstanaka@gmail.com>
2025-06-04 23:09:45 +0900

    🔁 lplaceweb に再アップ

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -26,7 +26,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "firebase-messaging-sw.js": "4e8302ed3196cdf02b055688585cd3d6",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "2f6f58c9baa56bc63b1fa9779b67db33",
+"flutter_bootstrap.js": "bacf7acd48c97995fd1cf57bdabb507e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
