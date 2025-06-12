33c5ca39ed4ad985144c42e85cfe340ee28a8e76
おさむ <zzzstanaka@gmail.com>
2025-06-12 17:37:59 +0900

    🔁 lplaceweb に再アップ

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "19545100"
+    serviceWorkerVersion: "1392605925"
   }
 });
